732d91ecacb1216625f00182560febca86029210
juan_carlos_meza <juancmezat@gmail.com>
2021-02-08 22:01:33 -0800

    not dry but working

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,38 +1,31 @@
 import React from 'react'
-
 const MenuBar = (props) => {
-
   /*
-
   The 'a' tags below are the menu items. Think about the way a menu 
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
   on a menu item? Do we need state in this component, and if not, how can
   this component be made aware of what is currently the active menu item?
-
   */
-
+  function handleClick(e) {
+    props.displayProfile(e.target.id)
+  }
   return (
     <div className="ui four item menu">
-      <a onClick={props.handleClick} className="item active" id="profile">
+      <a className="item active" id="profile" onClick={handleClick}>
         <i className="user large icon" id="profile"/>
       </a>
-
-      <a onClick={props.handleClick} className="item" id="photo">
+      <a className="item" id="photo" onClick={handleClick}>
         <i className="photo large icon" id="photo"/>
       </a>
-
-      <a onClick={props.handleClick} className="item" id="cocktail">
+      <a className="item" id="cocktail" onClick={handleClick}>
         <i className="cocktail large icon" id="cocktail"/>
       </a>
-
-      <a onClick={props.handleClick} className="item" id="pokemon"> 
+      <a className="item" id="pokemon" onClick={handleClick}> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
   )
-
 }
-
 export default MenuBar
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -1,58 +1,62 @@
 import React from 'react'
 import MenuBar from '../components/MenuBar.js'
 import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
-
 class MainBox extends React.Component {
-
-
-  state = {
-    itemSelected: <Profile />
-  }
-
-  handleClick = (e) => {
-    debugger
-    switch(e.target.id) {
-      case 'profile':
-        let profile = <Profile />
-        this.setState({
-          itemSelected: profile
-        })
-      case "photo":
-        let photo = <Photos />
-        this.setState({
-          itemSelected: photo
-        })
-      case "cocktail":
-        let cocktails = <Cocktails />
-        this.setState({
-          itemSelected: cocktails
-        })      
-      case "pokemon":
-        let pokemon = <Pokemon />
-        this.setState({
-          itemSelected: pokemon
-        })      
-    }
-  }
-
+ state={
+   profile: false,
+   photo: false,
+   cocktails: false,
+   pokemon: false
+ }
+ displayProfile = (id) => {
+   if(id === 'profile'){
+    let prevProfile = this.state.profile
+      this.setState({
+        profile: !prevProfile,
+        photo: false,
+        cocktails: false,
+        pokemon: false
+      })
+   } else if (id === 'photo'){
+     let prevPhoto = this.state.photo
+     this.setState({
+       photo: !prevPhoto,
+       profile: false,
+       cocktails: false,
+       pokemon: false
+     })
+   } else if (id === 'cocktail'){
+     let prevCocktails = this.state.cocktails
+     this.setState({
+       cocktails: !prevCocktails,
+       profile: false,
+       photo: false,
+       pokemon: false
+     })
+   } else if (id === 'pokemon'){
+     let prevPokemon = this.state.pokemon
+     this.setState({
+       pokemon: !prevPokemon,
+       profile: false,
+       photo: false,
+       cocktails: false
+     })
+   }
+ }
   render() {
-
     /*
-
     Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
-
     */
-
-    const detailsToDisplay = <div>{this.state.itemSelected}</div>
-
+    // const detailsToDisplay = <div>{this.state.profile == true ? <Profile /> : null}</div>
     return (
       <div>
-        <MenuBar handleClick={this.handleClick}/>
-        {detailsToDisplay}
+        <MenuBar profile={this.state.profile} photo={this.state.photo} cocktails={this.state.cocktails} pokemon={this.state.pokemon} displayProfile={this.displayProfile}/>
+        {this.state.profile === true ? <Profile /> : null}
+        {this.state.photo === true ? <Photos /> : null}
+        {this.state.cocktails === true ? <Cocktails /> : null}
+        {this.state.pokemon === true ? <Pokemon /> : null}
       </div>
     )
   }
-
 }
-
 export default MainBox
